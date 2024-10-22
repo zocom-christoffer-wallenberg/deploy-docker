6bbc2ff1ec3b183c1b0ccb0c4d4605ae55ab5fd8
zocom-christoffer-wallenberg <54267140+zocom-christoffer-wallenberg@users.noreply.github.com>
2024-10-22 15:46:30 +0200

    Added code

---
+++ src/router/router.jsx
@@ -0,0 +1,34 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import Events from "../pages/Events";
+import SelectedEvent from "../pages/SelectedEvent";
+import Confirmation from "../pages/Confirmation";
+
+function isProduction() {
+  console.log(import.meta.env);
+
+  if (import.meta.env.PROD) return { basename: "/test" };
+  else return {};
+}
+
+const basename = isProduction();
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/", // / är startsidan
+      element: <Events />,
+    },
+    {
+      path: "/event",
+      element: <SelectedEvent />,
+    },
+    {
+      path: "/confirmation",
+      element: <Confirmation />,
+    },
+  ],
+  basename
+);
+
+export default router;
